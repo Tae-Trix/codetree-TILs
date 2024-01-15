513ee75b45cba0ce397bb980af23d25fb6fe4645
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-15 12:54:55 +0900

    Title: 세 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240115/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240115/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -4,11 +4,19 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 
-if a<=b and a<=c:
+if a<b and a<c:
     print(a)
-
-if b<=c and b<=a:
+if b<c and b<a:
     print(b)
-
-if c<=a and c<=b:
+if c<a and c<b:
     print(c)
+
+if a==b and a!=c:
+    print(a)
+if b==c and b!=a:
+    print(b)
+if c==a and c!=b
+    print(c)
+
+if a==b and b==c and c==a:
+    print(a)
