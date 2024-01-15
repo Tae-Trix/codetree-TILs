fcaac279984ccab96df5bd6fb049015dec7bc9c2
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-15 16:24:07 +0900

    Title: 윤년인가, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240115/윤년인가/is-leap-year.py
@@ -0,0 +1,16 @@
+y = int(input())
+
+answer = "true"
+
+if y%4==0:
+    answer = 'true'
+    if y%100==0:
+        answer = 'false'
+        if y%400==0:
+            answer = 'true'
+        else:
+            answer = 'true'
+else:
+    answer = 'true'
+
+print(answer)
