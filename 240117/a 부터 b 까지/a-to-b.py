4ba4c1bfba114c40a764d103ddb3a806f4f8591a
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-17 15:14:00 +0900

    Title: a 부터 b 까지, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240117/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,11 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+while a<=b:
+    if a%2==1:
+        print(a, end = ' ')
+        a = a*2
+    elif a%2==0:
+        print(a, end = ' ')
+        a = a+3
