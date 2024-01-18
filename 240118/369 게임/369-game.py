8ab66e341f2d4a315e2f056b8fce4af45452da49
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-18 16:17:33 +0900

    Title: 369 게임, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/369 게임/369-game.py
+++ 240118/369 게임/369-game.py
@@ -4,8 +4,8 @@ for i in range(1,n+1):
     if i % 3 == 0:
         print(0, end = ' ')
     elif (i % 10 == 3) or (i % 10 == 6) or (i % 10 == 9):
-        print(0)
+        print(0, end = ' ')
     elif (i // 10 == 3) or (i // 10 == 6) or (i // 10 == 9):
-        print(0)
+        print(0, end = ' ')
     else:
         print(i, end = ' ')
