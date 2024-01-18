2d257d66fd7bc62edc612cd7c614a302618700a1
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-18 17:02:19 +0900

    Title: a/b 출력, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/a/b 출력/a-divide-b.py
+++ 240118/a/b 출력/a-divide-b.py
@@ -12,3 +12,11 @@ if a//b == 0:
         a = (a*10)%b
         print(int(c), end = '')
         i = i + 1
+elif a//b == 1:
+    print(1, end = '')
+    print(".", end = '')
+    while i <= 20:
+        c = (a*10)//b
+        a = (a*10)%b
+        print(int(c), end = '')
+        i = i + 1
