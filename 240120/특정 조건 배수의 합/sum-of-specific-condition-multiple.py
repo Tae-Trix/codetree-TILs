aa0ba3d89d4a744866ee8800127ba7a4c0ae0124
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-20 21:35:34 +0900

    Title: 특정 조건 배수의 합, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240120/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -3,8 +3,15 @@ a = int(arr[0])
 b = int(arr[1])
 
 sum_val = 0
-for i in range(a, b+1):
-    if i%5==0:
-        sum_val = sum_val + i
+
+if a<b:
+    for i in range(a, b+1):
+        if i%5==0:
+            sum_val = sum_val + i
+elif a>b:
+    for i in range(b, a+1):
+        if i%5==0:
+            sum_val = sum_val + i
+        
 
 print(sum_val)
