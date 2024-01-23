3f7cf3f259a48638ad0e0dbef8ed3d7ccc968d81
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-23 17:15:27 +0900

    Title: 3n + 1 수열, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/3n + 1 수열/3n-plus-1-sequence.py
+++ 240123/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,15 +1,16 @@
 n = int(input())
 num = 0
 while True:
-    num = num + 1
-    if n%2==0:
+    if n==1:
+        break
+    elif n%2==0:
         n = n/2
+        num = num + 1
         if n==1:
             break
     elif n%2==1:
         n = 3*n + 1
+        num = num + 1
         if n==1:
             break
-    elif n==1:
-        break
 print(num)
