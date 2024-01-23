eb2053eba4e0f71ac135904598d6802037b5d4ee
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-23 16:39:45 +0900

    Title: 출석 부르기 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    n = int(input())
+    if n>=5 and n<=100:
+        print("Vacancy")
+        break
+    elif n == 1:
+        print("John")
+    elif n == 2:
+        print("Tom")
+    elif n == 3:
+        print("Paul")
+    elif n == 4:
+        print("Sam")
