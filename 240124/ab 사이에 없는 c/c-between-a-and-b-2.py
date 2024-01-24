5af848d68b32a03fdcdb1b5fc9132c46dcb07481
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-24 17:20:39 +0900

    Title: ab 사이에 없는 c, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240124/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -2,11 +2,11 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
-satisfied = True
+satisfied = False
 
 for i in range(a, b+1):
-    if i%c != 0:
-        satisfied = False
+    if i%c == 0:
+        satisfied = True
 
 if satisfied == False:
     print("YES")
