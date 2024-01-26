7bf247586d3ff4295cb3f74e0f1fe3c927ce3ef5
Taesu Kim <155162378+Tae-Trix@users.noreply.github.com>
2024-01-26 21:16:09 +0900

    Title: 별표 출력하기 9, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/별표 출력하기 9/print-star-9.py
+++ 240126/별표 출력하기 9/print-star-9.py
@@ -3,6 +3,6 @@ n = int(input())
 for i in range(0,n):
     for j in range(0,n-(i+1)):
         print('  ', end = '')
-    for j in range(0, n+i+(i-3)):
+    for j in range(0, 2*i+1):
         print('*', end = ' ')
     print()
